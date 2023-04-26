ff008b94704649ace2d57493fba124cbcbbe9b0e
11jl98 <joaoferreira981011@gmail.com>
2023-04-26 11:03:50 -0300

    alteração de versão do pacote

--- dist/src/configs/database/database.js
+++ dist/src/configs/database/database.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.database = void 0;
-var knex_1 = __importDefault(require("knex"));
+const knex_1 = __importDefault(require("knex"));
 require("dotenv/config");
 exports.database = (0, knex_1.default)({
     client: 'mysql2',
--- dist/src/utils/middleware/auth.middleware.js
+++ dist/src/utils/middleware/auth.middleware.js
@@ -4,15 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.authMiddlewares = void 0;
-var jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 function authMiddlewares(request, response, next) {
-    var authorization = request.headers.authorization;
+    const { authorization } = request.headers;
     if (!authorization)
         return response.sendStatus(401);
-    var token = authorization.replace("Bearer", "").trim();
+    const token = authorization.replace("Bearer", "").trim();
     try {
-        var data = jsonwebtoken_1.default.verify(token, process.env.PASSWORD_JWT);
-        var _a = data, id = _a.id, rule = _a.rule;
+        const data = jsonwebtoken_1.default.verify(token, process.env.PASSWORD_JWT);
+        const { id, rule } = data;
         request.id_user = id;
         request.rule_user = rule;
         next();
