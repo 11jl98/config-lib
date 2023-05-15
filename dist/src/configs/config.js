f96ef70504ec044fee02357fd58d19e99aa70797
11jl98 <joaoferreira981011@gmail.com>
2023-05-15 17:24:57 -0300

    commit

--- dist/src/configs/config.js
+++ dist/src/configs/config.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 exports.config = {
     microservices: {
-        auth: `${process.env.BASE_URL}/${process.env.PORT_AUTH}`,
-        users: `${process.env.BASE_URL}/${process.env.PORT_USER}`,
-        subscribeProducts: `${process.env.BASE_URL}/${process.env.PORT_SUBSCRIBED_PRODUCTS}`,
+        auth: `${process.env.BASE_URL}:${process.env.PORT_AUTH}`,
+        users: `${process.env.BASE_URL}:${process.env.PORT_USER}`,
+        subscribeProducts: `${process.env.BASE_URL}:${process.env.PORT_SUBSCRIBED_PRODUCTS}`,
     },
 };
--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,7 +1,7 @@
  export const config = {
     microservices: {
-        auth: `${process.env.BASE_URL}/${process.env.PORT_AUTH}`,
-        users: `${process.env.BASE_URL}/${process.env.PORT_USER}`,
-        subscribeProducts: `${process.env.BASE_URL}/${process.env.PORT_SUBSCRIBED_PRODUCTS}`,
+        auth: `${process.env.BASE_URL}:${process.env.PORT_AUTH}`,
+        users: `${process.env.BASE_URL}:${process.env.PORT_USER}`,
+        subscribeProducts: `${process.env.BASE_URL}:${process.env.PORT_SUBSCRIBED_PRODUCTS}`,
       },
  }
